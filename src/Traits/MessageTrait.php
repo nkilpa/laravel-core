ab15422d0af903e52fd57050c8133c510a4de1db
nikitakilpa <nikitakilpa@gmail.com>
2022-03-14 14:46:52 +0300

    Создал консольный метод, багфикс и оптимизация кода

--- src/Traits/MessageTrait.php
+++ src/Traits/MessageTrait.php
@@ -4,15 +4,26 @@ namespace nikitakilpa\Core\Traits;
 
 trait MessageTrait
 {
-    private $errors = [];
+    private array $errors = [];
+    private array $messages = [];
 
     public function addErrors($errors)
     {
         $this->errors = $errors;
     }
 
-    public function getErrors()
+    public function getErrors(): array
     {
         return $this->errors;
+    }
+
+    public function addMessages($messages)
+    {
+        $this->messages = $messages;
+    }
+
+    public function getMessages(): array
+    {
+        return $this->messages;
     }
 }
